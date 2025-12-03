52bc5eb8a9486684c4292ab8e5dd7ca4d653ee2d
yves147 <yves.j@outlook.de>
2025-12-03 15:46:37 +0100

    seperated greedy

--- day3/task2.hs
+++ day3/task2.hs
@@ -1,14 +1,15 @@
+greedy :: Ord a => Int -> [a] -> [a]
+greedy 0 _ = []
+greedy _ [] = []
+greedy n xs = 
+  let remaining = n - 1
+      bdelta = take (length xs - remaining) xs -- search space
+      delta = maximum bdelta
+      tau = length (takeWhile (/= delta) bdelta) -- index of delta
+  in delta : greedy (n - 1) (drop (tau + 1) xs)
+
 joltage :: String -> Integer
 joltage line = read (greedy 12 line)
-  where
-    greedy 0 _ = []
-    greedy _ [] = []
-    greedy n xs = 
-      let remaining = n - 1
-          bdelta = take (length xs - remaining) xs -- search space
-          delta = maximum bdelta
-          tau = length (takeWhile (/= delta) bdelta) -- index of delta
-      in delta : greedy (n - 1) (drop (tau + 1) xs)
 
 main :: IO ()
 main = do
